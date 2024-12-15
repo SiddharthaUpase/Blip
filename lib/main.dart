ac5e55536e78b3504e6c034186ce67336baa0a65
SiddharthaUpase <s1dupase34@gmail.com>
2024-12-15 07:23:26 +0530

    Change initial route from HOME to LOGIN in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: Routes.HOME,
+      initialRoute: Routes.LOGIN,
       getPages: AppPages.routes,
     );
   }
